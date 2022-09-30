8b46d719fd184933c70aee884aa01e3b39967364
jcruzap <jcruzap95@gmail.com>
2022-09-30 02:50:07 -0300

    Item Collection usando Firebase

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartContextProvider } from './context/CartContext';
+import Cart from './components/Cart/Cart'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <CartContextProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer saludo="Todos nuestros productos" />} />
+            <Route path='categorias/:idCategorias' element={<ItemListContainer />} />
+            <Route path='/detalles/:idProductos' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='*' element={<h1>Estamos procesando su compra..</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </CartContextProvider>
+
+    </div>
+  );
+}
+
+export default App;
